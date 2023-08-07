e37f302aeb2cff4f14511eacbc12cead408945c1
ChakradharBinnar <128061840+ChakradharBinnar@users.noreply.github.com>
2023-08-08 01:11:13 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,6 +2,6 @@
 import mysql.connector as con
 
 mydb = con.connect(host='localhost', user='root',
-                   password='Binnar@123', database='data1')
+                   password='*******', database='data1')
 
 print(mydb, "Successfully connnection establish !")
